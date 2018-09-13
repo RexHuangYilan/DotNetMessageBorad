88fe669dbe3e2f6259af8465cf42135a92ec6646
Rex.Huang <rex.huang@ehsn.com.tw>
2018-09-13 15:02:26 +0800

    新增 留言板 db 建立

---
+++ WebApplicationRexMessageBoard/DAL/MessageBoardContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WebApplicationRexMessageBoard.Models;
+
+namespace WebApplicationRexMessageBoard.DAL
+{
+    public class MessageBoardContext : DbContext
+    {
+        public MessageBoardContext() : base("RexMessageBoard")
+        {
+        }
+
+        public DbSet<MessageBoardModel> MessageBoards { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<ReplyModels>().HasKey(d => new { d.MessageBoard, d.Message });
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
