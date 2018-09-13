b04476459ceed70c5d5cd55c5e4f971b5cb4bab6
Rex.Huang <rex.huang@ehsn.com.tw>
2018-09-13 15:00:16 +0800

    新增 留言版 model

---
+++ WebApplicationRexMessageBoard/Models/MessageModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationRexMessageBoard.Models
+{
+    public class MessageModels
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual ICollection<ReplyModels> Reply { get; set; }
+    }
+}
---
+++ WebApplicationRexMessageBoard/Models/ReplyModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationRexMessageBoard.Models
+{
+    public class ReplyModels
+    {
+        public int ID { get; set; }
+
+        [Key]
+        [Required]
+        public virtual MessageBoardModel MessageBoard { get; set; }
+
+        [Key]
+        [Required]
+        public virtual MessageModels Message { get; set; }
+    }
+}
