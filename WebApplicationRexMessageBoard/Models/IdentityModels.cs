f1fc80e3ece593e663aa5ebeb8efebbcf081c7de
Rex.Huang <rex.huang@ehsn.com.tw>
2018-09-13 15:03:43 +0800

    修改 indentity 預設 db 與新增欄位

--- WebApplicationRexMessageBoard/Models/IdentityModels.cs
+++ WebApplicationRexMessageBoard/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,9 @@ namespace WebApplicationRexMessageBoard.Models
     // 您可將更多屬性新增至 ApplicationUser 類別，藉此為使用者新增設定檔資料，如需深入了解，請瀏覽 https://go.microsoft.com/fwlink/?LinkID=317594。
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<MessageBoardModel> MessageBoard { get; set; }
+        public virtual ICollection<MessageModels> Message { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // 注意 authenticationType 必須符合 CookieAuthenticationOptions.AuthenticationType 中定義的項目
@@ -21,7 +25,7 @@ namespace WebApplicationRexMessageBoard.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("RexMessageBoard", throwIfV1Schema: false)
         {
         }
 
