be6ddec1a595bfafc2178510f2c806894cd766a3
Rex.Huang <rex.huang@ehsn.com.tw>
2018-09-25 10:28:40 +0800

    修改 留言呈現標題

--- WebApplicationRexMessageBoard/Models/MessageBoardModel.cs
+++ WebApplicationRexMessageBoard/Models/MessageBoardModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,10 +12,12 @@ namespace WebApplicationRexMessageBoard.Models
     {
         public int ID { get; set; }
 
+        [DisplayName("留言標題")]
         [MaxLength(50)]
         [Required]
         public string Title { get; set; }
 
+        [DisplayName("留言內容")]
         [MaxLength(255)]
         [Required]
         public string Content { get; set; }
--- WebApplicationRexMessageBoard/Models/MessageModels.cs
+++ WebApplicationRexMessageBoard/Models/MessageModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace WebApplicationRexMessageBoard.Models
     {
         public int ID { get; set; }
 
+        [DisplayName("回覆內容")]
         [MaxLength(255)]
         [Required]
         public string Content { get; set; }
