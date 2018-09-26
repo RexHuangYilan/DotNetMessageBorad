d2abcc267906ce50c45479e2f3789abef16ebcdd
Rex.Huang <rex.huang@ehsn.com.tw>
2018-09-26 09:41:01 +0800

    修改 回覆頁的刪除功能
    改為以 AJAX 方式呼叫

--- WebApplicationRexMessageBoard/Scripts/messageFunction.js
+++ WebApplicationRexMessageBoard/Scripts/messageFunction.js
@@ -6,13 +6,33 @@
             return;
         }
         
+        var url = 'Delete/' + id;
+        $.fn.deleteMessageAJAX(url, element, token);
+    };
+
+    $.fn.deleteReply = function (id, element, token) {
+
+        if (!id) {
+            return;
+        }
+
+        var url = '../MessageDeleteAJAX/' + id;
+        $.fn.deleteMessageAJAX(url, element, token);
+    };
+
+    $.fn.deleteMessageAJAX = function (url, element, token) {
+
+        if (!url) {
+            return;
+        }
+
         var reply = confirm('確認刪除留言?')
         if (!reply) {
             return;
         }
 
         $.ajax({
-            url: 'Delete/'+id,
+            url: url,
             type: 'POST',
             data: {
                 __RequestVerificationToken: token
@@ -26,7 +46,6 @@
                 if (element) {
                     element.remove();
                 }
-                
             }
         });
     };
